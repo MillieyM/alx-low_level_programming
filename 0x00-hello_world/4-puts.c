614898eb03fefccba70a2d9119cf4b0af947bfa0
MillieyM <millieymafune@gmail.com>
2024-06-23 08:51:44 -0700

    C program that prints a string, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    puts("\"Programming is like building a multiligual puzzle");
-      return (0);
+ 	puts("\"Programming is like building a miltilingual puzzle");
+     	return (0);
 }
